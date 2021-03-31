d459e3be13bc33bf4f08b17665489f779fb3650e
Федерягин Андрей <eniluck@mail.ru>
2021-03-31 09:45:43 +0500

    4/8

--- README.md
+++ README.md
@@ -1,27 +1,4 @@
-# tutorual_online_shop
-
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Пример Vue приложения с канала (Go Frontend)[https://www.youtube.com/channel/UCWhg5tJHcR22vlJ5QVz7opg]
 
 Подключенные библиотеки
 - Axios 0.19.0: https://github.com/axios/axios​
@@ -29,6 +6,7 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 - Json-server (fakse-api): https://github.com/typicode/json-server​
 - Node-sass 4.13.0: https://github.com/sass/node-sass​
 - Sass-loader 8.0.0: https://www.npm.red/package/sass-loader​ (устанавливает так же Node-sass)
+
 
 Используемые расширения для Chrome
 - Vue.js devtools
--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -3,7 +3,7 @@
       <h1>Catalog</h1>
       <div class="v-catalog__list">
         <v-catalog-item 
-            v-for="product in products"
+            v-for="product in PRODUCTS"
             :key="product.article"
             :product_data="product"
             @sendArticle ="showChildArticleInConsole"
@@ -14,6 +14,7 @@
 
 <script>
 import vCatalogItem from './v-catalog-item'
+import {mapActions, mapGetters} from 'vuex'
 
 export default {
     name: "v-catalog",
@@ -23,63 +24,29 @@ export default {
     props: {},
     data() {
         return {
-            products: [
-                {
-                    image: "1.jpg",
-                    name: "T-shirt 1",
-                    price: 2100,
-                    article: "T1",
-                    available: true,
-                    category: "Мужские"
-                },
-                {
-                    image: "2.jpg",
-                    name: "T-shirt 2",
-                    price: 3150,
-                    article: "T2",
-                    available: true,
-                    category: "Женские"
-                },
-                {
-                    image: "3.jpg",
-                    name: "T-shirt 3",
-                    price: 4200,
-                    article: "T3",
-                    available: false,
-                    category: "Женские"
-                },
-                {
-                    image: "4.jpg",
-                    name: "T-shirt 4",
-                    price: 5300,
-                    article: "T4",
-                    available: true,
-                    category: "Мужские"
-                },
-                {
-                    image: "5.jpg",
-                    name: "T-shirt 5",
-                    price: 6500,
-                    article: "T5",
-                    available: false,
-                    category: "Женские"
-                },
-                {
-                    image: "6.jpeg",
-                    name: "T-shirt 6",
-                    price: 8700,
-                    article: "T6",
-                    available: true,
-                    category: "Женские"
-                }
-            ]
+             
         }
     },
-    computed: {},
+    computed: {
+        ...mapGetters([
+            'PRODUCTS'
+        ])
+    },
     methods: {
+        ...mapActions([
+            'GET_PRODUCTS_FROM_API'
+        ]),
         showChildArticleInConsole(data){
             console.log(data);
         }
+    },
+    mounted(){
+        this.GET_PRODUCTS_FROM_API()
+        .then((response) => {
+            if(response.data) {
+                console.log('Data arrived')
+            }
+        })
     }
 
 }
