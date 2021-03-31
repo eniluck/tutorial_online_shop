a981f1a62b86b7644cf3dc828d7accd7db37e16b
Федерягин Андрей <eniluck@mail.ru>
2021-03-31 12:25:26 +0500

    6/8

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="v-catalog">
+      <router-link :to="{name: 'cart', params: {cart_data: CART}}">
+        <div class="v-catalog__link_to_cart">Cart: {{CART.length}}</div>
+      </router-link>
       <h1>Catalog</h1>
       <div class="v-catalog__list">
         <v-catalog-item 
@@ -29,7 +32,8 @@ export default {
     },
     computed: {
         ...mapGetters([
-            'PRODUCTS'
+            'PRODUCTS',
+            'CART'
         ])
     },
     methods: {
@@ -60,6 +64,13 @@ export default {
             flex-wrap: wrap;
             justify-content: space-between;
             align-items: center;
+        }
+        &__link_to_cart{
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            padding: $padding*2;
+            border: solid 1px grey;
         }
     }
 
--- src/components/v-main-wrapper.vue
+++ src/components/v-main-wrapper.vue
@@ -1,24 +1,15 @@
 <template>
     <div class="v-main-wrapper">
-         <v-catalog></v-catalog>
-         <v-cart 
-            v-if="CART.length"
-            :cart_data="CART"
-         />
+        <keep-alive>
+            <router-view></router-view>
+        </keep-alive>
     </div>
 </template>
 
 <script>
-    import vCatalog from './v-catalog'
-    import vCart from './v-cart'
-    import {mapGetters} from 'vuex'
 
     export default {
         name : 'v-main-wrapper',
-        components: {
-            vCatalog,
-            vCart
-        },
         props: {
 
         },
@@ -27,16 +18,9 @@
                 title : 'Main wrapper'
             }
         },
-        computed: {
-            ...mapGetters([
-                'CART'
-            ])
-        },
+        computed: {},
         methods: {},
-        watch: {},
-        mounted() {
-            console.log('Hello i am alive')
-        }
+        watch: {}
     }
 </script>
 
